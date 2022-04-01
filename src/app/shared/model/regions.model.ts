83bfe8af043b2007700e093fca844a3564f1c6d3
unknown <felicien.haraux@hotmail.fr>
2022-04-01 16:57:27 +0200

    oublie

--- src/app/shared/model/regions.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Region {
-    nom: string;
-    code: string;
-    _score: number;
-}
-
-export function instanceOfRegion(object: any): object is Region{
-    return 'nom' in object && 'code' in object && '_score' in object;
-}
